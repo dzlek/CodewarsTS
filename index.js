a1267732433453d9955a7896f629e8ae65144180
dzlek <6894255@gmail.com>
2025-05-15 14:57:00 +0300

    feat: task

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 "use strict";
 console.log('started....');
+//1 https://www.codewars.com/kata/515e271a311df0350d00000f/train/typescript
+const arr1 = [1, 2, 2];
+function squareSum(arr1) {
+    return arr1.map((val) => val * val).reduce((acc, cur) => acc + cur, 0);
+}
+console.log(squareSum(arr1));
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
 console.log('started....');
+
+//1 https://www.codewars.com/kata/515e271a311df0350d00000f/train/typescript
+
+const arr1 = [1, 2, 2];
+
+function squareSum(arr1: number[]): number {
+    return arr1.map((val) => val*val).reduce((acc, cur) => acc + cur, 0);
+}
+
+console.log(squareSum(arr1));
