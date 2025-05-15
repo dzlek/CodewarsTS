eccbf83faaea95a1d34e41eb566325ea4dab70f8
dzlek <6894255@gmail.com>
2025-05-15 18:22:23 +0300

    feat: task

--- index.ts
+++ index.ts
@@ -2,10 +2,19 @@ console.log('started....');
 
 //1 https://www.codewars.com/kata/515e271a311df0350d00000f/train/typescript
 
-const arr1 = [1, 2, 2];
+// const arr1 = [1, 2, 2];
 
-function squareSum(arr1: number[]): number {
-    return arr1.map((val) => val*val).reduce((acc, cur) => acc + cur, 0);
+// function squareSum(arr1: number[]): number {
+//     return arr1.map((val) => val*val).reduce((acc, cur) => acc + cur, 0);
+// }
+
+// console.log(squareSum(arr1));
+
+//2 https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/train/typescript
+
+function reverseWords(str: string): string {
+  // your code here
+  return str.split(' ').map(el=>el.split('').reverse().join('')).join(' ');
 }
 
-console.log(squareSum(arr1));
+console.log(reverseWords('The quick brown fox jumps over the lazy dog.'));
