b3203ede1fa7bb66086223bf0e06ec353d97b43b
dzlek <6894255@gmail.com>
2025-05-16 12:19:49 +0300

    feat: task

--- index.js
+++ index.js
@@ -7,8 +7,18 @@ console.log('started....');
 // }
 // console.log(squareSum(arr1));
 //2 https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/train/typescript
-function reverseWords(str) {
-    // your code here
-    return str.split(' ').map(el => el.split('').reverse().join('')).join(' ');
+// function reverseWords(str: string): string {
+//   // your code here
+//   return str.split(' ').map(el=>el.split('').reverse().join('')).join(' ');
+// }
+// console.log(reverseWords('The quick brown fox jumps over the lazy dog.'));
+//3 https://www.codewars.com/kata/517abf86da9663f1d2000003/train/typescript
+function toCamelCase(str) {
+    const arr = str.split('-').join(' ').split('_').join(' ').split(' ');
+    const firstWorld = arr[0];
+    const restWorldstoUppercace = arr.slice(1).map(el => {
+        return el[0].toUpperCase() + el.slice(1);
+    });
+    return String([firstWorld, ...restWorldstoUppercace].join(''));
 }
-console.log(reverseWords('The quick brown fox jumps over the lazy dog.'));
+console.log(toCamelCase("The-stealth_warrior"));
